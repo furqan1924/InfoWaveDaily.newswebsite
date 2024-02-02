9e4b0088d52a63851ffa768201fac16e5b571719
Muhammad Furqan <furqan1924@gmail.com>
2024-02-03 00:35:18 +0500

    setup enviorment variable

--- src/Componenets/News.js
+++ src/Componenets/News.js
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react'
 import { Pagination } from 'antd';
 
 export default function News(props) {
+  const REACT_APP_BASE_API_KEY_URL = process.env.REACT_APP_BASE_API_KEY_URL;
+  const REACT_APP_MIDDLE_API_KEY_URL = process.env.REACT_APP_MIDDLE_API_KEY_URL;
+  const REACT_APP_LAST_API_URL = process.env.REACT_APP_LAST_API_URL
 
   const [articles, setArticles] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -24,7 +27,7 @@ export default function News(props) {
 
   const Updatenews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=ae6f87f59e7448fa9e9465c013471a00`;
+    const url = `${REACT_APP_BASE_API_KEY_URL}${props.country}${REACT_APP_MIDDLE_API_KEY_URL}${props.category}${REACT_APP_LAST_API_URL}`;
 
     const data = await fetch(url);
     props.setProgress(60);
@@ -44,20 +47,20 @@ export default function News(props) {
     <>
 
       <h1 className='text-center my-5' style={{}}>InfoWaveDaily - {capitalizeFirstLowercaseRest(str)}</h1>
-     
-        <div className='container my-3'>
-          <div className='row'>
-            {
-              currentRecords.map((element) => {
-                return <div className='col-md-4' key={element.url}>
-                  <Newsitems title={element.title} description={element.description} urlToImage={element.urlToImage} author={element.author} date={element.publishedAt} source={element.source.name} url={element.url} />
-                </div>
-              })
-            }
-          </div>
-          <Pagination className='mt-5' onChange={(nPages) => { setCurrentPage(nPages) }} defaultCurrent={currentPage} total={`${nPages}` + 0} />
+
+      <div className='container my-3'>
+        <div className='row'>
+          {
+            currentRecords.map((element) => {
+              return <div className='col-md-4' key={element.url}>
+                <Newsitems title={element.title} description={element.description} urlToImage={element.urlToImage} author={element.author} date={element.publishedAt} source={element.source.name} url={element.url} />
+              </div>
+            })
+          }
         </div>
-   
+        <Pagination className='mt-5' onChange={(nPages) => { setCurrentPage(nPages) }} defaultCurrent={currentPage} total={`${nPages}` + 0} />
+      </div>
+
 
     </>
   )
